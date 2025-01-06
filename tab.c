76c72ef902039f9083ecfff60de1ef683f224cc8
Janet Tong <jtong50@stuy.edu>
2025-01-06 16:17:55 -0500

    rm display, tab.c functions

---
+++ tab.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// arguments: name of tab and dimensions for new table
+// returns void 
+// creates a new table/tab 
+void create_tab(char* name, int rows, int cols){
+
+}
+
+// no arguments 
+// returns void 
+// displays contents of table in spreadsheet format 
+void display_tab(){
+
+}
+
+// no arguments 
+// returns void 
+// displays all available tables with their names and dimensions
+void tab_list(){
+
+}
+
+// arguments: name of table to access
+// returns void
+// display contents of accessed tab, prompt user to read/write
+void access_tab(char* name){
+
+}
+
+// arguments: name of table to delete 
+// returns void 
+// deletes specified table and displays updated tab list
+void delete_tab(char* name){
+
+}
