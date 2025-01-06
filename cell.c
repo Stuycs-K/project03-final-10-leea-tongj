09849a1f30bea0e44149d3c7a6e58e135d8bc309
Janet Tong <jtong50@stuy.edu>
2025-01-06 16:08:25 -0500

    added files, cell.c functions:

---
+++ cell.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// arguments: row, col of cell and input for cell
+// returns void 
+// udpates specified cell with input
+void update_cell(int row, int col, char * input){
+
+}
+
+// arguments: row, col of cell
+// returns the cleared input 
+// prompts user for row and col of cell to modify and clear the cell's input
+char * clear_cell(int row, int col){
+
+}
+
+// no arguments
+// returns void 
+// adds a row to the end of spreadsheet and prompts user for input
+void add_row(){
+
+}
+
+// no arguments
+// returns void 
+// adds a column to the spreadsheet and prompts user for input
+void add_column(){
+
+}
