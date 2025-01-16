18a05a4eb8139501cda6600840c704f93142d6bc
Janet Tong <jtong50@stuy.edu>
2025-01-15 23:39:04 -0500

    home, view, and import in lst_func

--- user.c
+++ user.c
@@ -18,12 +18,9 @@ void display_menu(struct table ** tbl_list, int home, int view, int select){
             strcat(menu, "[view_list] view table list \n"); 
         }
         if (view){
-            strcat(menu, "[select n] select table #n\n"); 
-        }
-        if (select){
-            strcat(menu, "[access] access the table in new window \n"); 
-            strcat(menu, "[delete] delete table \n"); 
-            strcat(menu, "[export] export table to file \n");
+            strcat(menu, "[access n] access nth table in new window \n"); 
+            strcat(menu, "[delete n] delete nth table \n"); 
+            strcat(menu, "[export n] export nth table to file \n");
         }
     }
     printf("%s", menu);
@@ -31,6 +28,38 @@ void display_menu(struct table ** tbl_list, int home, int view, int select){
 
 // prompts user with table list function options
 int table_lst_func(struct table ** tbl_lst){
-    printf("Welcome to the spreadsheet. Begin by typing in the prompt with a menu directive");
+    printf("Welcome to the spreadsheet. Begin by typing in the prompt with a menu directive\n");
     display_menu(tbl_lst, 0, 0, 0);
+    printf("Prompt: "); 
+    char buff[100]; 
+    fgets(buff, sizeof(buff), stdin); 
+    if (!strcmp(buff, "home")){
+        display_menu(tbl_lst, 1, 0, 0);
+    }
+
+    if (!strcmp(buff, "import")){
+        printf("Provide the path to the csv you'd like to import: "); 
+        char path[256]; 
+        fgets(path, sizeof(path), stdin); 
+        printf("Name the new file: "); 
+        char name[256]; 
+        fgets(name, sizeof(name), stdin); 
+        read_csv(name, path);
+    }
+
+    if (!strcmp(buff, "view_list")){
+        display_menu(tbl_lst, 0, 1, 0);
+        display_table_list(tbl_lst); 
+    }
+
+    // if (!strcmp(buff, "access")){
+    //     ncurses(); 
+    // }
+
+    // if (!strcmp(buff, "delete")){
+    //     printf("Before deletion, would you like to export this table? (Y/N)"); 
+    //     char val[5];
+    //     fgets(val, sizeof(char), stdin); 
+    //     delete_table(tbl_lst, place, strcmp(val, "Y"))
+    // }
 }
