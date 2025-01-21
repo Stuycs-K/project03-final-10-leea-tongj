9d44db3e1ec0523bc81dc807f08d4eea2fd787ff
Janet Tong <jtong50@stuy.edu>
2025-01-20 22:34:05 -0500

    wrote stat function

--- csv.c
+++ csv.c
@@ -66,5 +66,41 @@ void view_csv_file(char* name){
 }
 
 void view_csv_stats(char * name){
-    
+    char PATH[56] = "./"; 
+    strcat(PATH, name); 
+    struct stat * stat_buffer = malloc(sizeof(struct stat));
+    stat(PATH, stat_buffer); 
+    int size = stat_buffer->st_size; 
+    int st_uid = stat_buffer->st_uid; 
+    int st_gid = stat_buffer->st_gid; 
+    char* st_mode = conv(stat_buffer->st_mode);
+    printf("File Name: %s\n", name); 
+    printf("File Size: %d\n", size); 
+    printf("User ID: %d\n", st_uid);
+    printf("Group ID: %d\n", st_gid);
+    printf("File Permissions: %s", st_mode);
+}
+
+char* conv(long long n){
+  char*  str = malloc(sizeof(char) * 11); 
+  if (n & S_IRUSR) strcat(str, "r"); //owner read 
+  else strcat(str, "-"); 
+  if (n & S_IWUSR) strcat(str, "w"); //owner write 
+  else strcat(str, "-");
+  if (n & S_IXUSR) strcat(str, "x"); //owner exec 
+  else strcat(str, "-");
+  if (n & S_IRGRP) strcat(str, "r"); //group read 
+  else strcat(str, "-");
+  if (n & S_IWGRP) strcat(str, "w"); //group write 
+  else strcat(str, "-");
+  if (n & S_IXGRP) strcat(str, "x"); //group exec 
+  else strcat(str, "-");
+  if (n & S_IROTH) strcat(str, "r"); //other read 
+  else strcat(str, "-");
+  if (n & S_IWOTH) strcat(str, "w"); //other write 
+  else strcat(str, "-");
+  if (n & S_IXOTH) strcat(str, "r"); //other exec 
+  else strcat(str, "-");
+
+  return str; 
 }
